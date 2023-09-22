c091c2923fca7049eec9a87b4a9935de99464f8b
dexter68555 <46341564+dexter68555@users.noreply.github.com>
2023-09-22 15:12:20 +0800

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use soloud::*;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let sl = Soloud::default()?;
+    let mut wav = audio::Wav::default();
+
+    // Create an empty playlist (vector of audio file paths)
+    let mut playlist: Vec<&str> = Vec::new();
+
+    // Add audio files to the playlist (replace with your audio file)
+    playlist.push("HeyMama.mp3");
+    playlist.push("HeyMama.mp3");
+    playlist.push("HeyMama.mp3");
+
+    // Loop through the playlist and play each audio file
+    for audio_file in playlist {
+        println!("Playing {audio_file}");
+        wav.load(&std::path::Path::new(audio_file))?;
+    
+        sl.play(&wav);
+        while sl.voice_count() > 0 {
+            std::thread::sleep(std::time::Duration::from_millis(100));
+        }
+    }
+
+    println!("Playlist end.");
+
+    Ok(())
+}
